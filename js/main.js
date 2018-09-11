2b8a4b796c8bb5e06ba7f1328e32abb81d0702dd
superrabbitrun007` <244398764@qq.com>
2018-09-11 18:07:22 +0800

    完成餐厅详情页面的响应式布局

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZmlzaDY2NiIsImEiOiJjamx1cmlqa2wwbnByM3ByNjdwN2tncWd2In0.QTTHZa9wXReckprZfs28ww',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
